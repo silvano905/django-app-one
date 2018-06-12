d7541a5f22e56445608b511cc1c8ff8f65f797f5
silvano905 <40185518+silvano905@users.noreply.github.com>
2018-06-11 21:01:01 -0500

    Update settings.py

--- project_one/project_one/settings.py
+++ project_one/project_one/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'c495d^jh_h_*&0igkawb*f(^j*9y$ve3d(e%zid#6g=^z*a@+7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['silvano90.pythonanywhere.com']
 
 
 # Application definition
